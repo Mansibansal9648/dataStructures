6cc32477615355a7e419ea9d3a670bb081545dd9
mansibansal9648 <mansibansal12345@gmail.com>
2024-11-30 22:19:50 +0530

    insertion and traversing in linked list

---
+++ src/LinkedList/LinkedListImplementation.java
@@ -0,0 +1,32 @@
+package LinkedList;
+
+public class LinkedListImplementation {
+    private Node head;
+
+    public void add(int data) {
+        Node newNode = new Node(data);
+        if (head == null) {
+            head = newNode;
+        } else {
+            Node current = head;
+            while (current.next != null) {
+                current = current.next;
+            }
+            current.next = newNode;
+        }
+    }
+
+    public void traverse() {
+        if (head == null) {
+            System.out.println("List is Empty");
+        } else {
+            Node current = head;
+            while (current != null) {
+                System.out.print(current.data + " -> ");
+                current = current.next;
+            }
+            System.out.println("null");
+        }
+    }
+
+}
---
+++ src/LinkedList/LinkedListMain.java
@@ -0,0 +1,12 @@
+package LinkedList;
+
+public class LinkedListMain {
+    public static void main(String[] args) {
+        LinkedListImplementation linkedList = new LinkedListImplementation();
+        linkedList.add(3);
+        linkedList.add(5);
+        linkedList.add(15);
+        linkedList.add(9);
+        linkedList.traverse();
+    }
+}
