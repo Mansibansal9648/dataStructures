75e37eadd6c7325348557cbccea676ce499d97a1
mansibansal9648 <mansibansal12345@gmail.com>
2024-12-07 00:28:12 +0530

    deletion in circular linked list

--- src/LinkedList/CircularLinkedList/CircularLinkedListImpl.java
+++ src/LinkedList/CircularLinkedList/CircularLinkedListImpl.java
@@ -36,4 +36,35 @@ public class CircularLinkedListImpl {
         }
     }
 
+    public void delete(int data){
+        if (head == null) {
+            System.out.println("List is Empty");
+            return;
+        }
+           Node current=head;
+        Node prev=null;
+        do{
+            if(current.data==data) {
+                if (prev == null) {
+                    if (head == tail) {
+                        head = null;
+                        tail = null;
+                    } else {
+                        head = head.next;
+                        tail.next = head;
+                    }
+                } else {
+                    prev.next = current.next;
+                    if(current==tail){
+                        tail=prev;
+                    }
+                }
+                return;
+            }
+            prev=current;
+            current=current.next;
+        }while(current!=head);
+        System.out.println("No element found in list");
+    }
+
 }
--- src/LinkedList/CircularLinkedList/CircularLinkedListMain.java
+++ src/LinkedList/CircularLinkedList/CircularLinkedListMain.java
@@ -4,10 +4,16 @@ public class CircularLinkedListMain {
     public static void main(String[] args) {
         CircularLinkedListImpl circularLinkedList=new CircularLinkedListImpl();
         circularLinkedList.display();
+        circularLinkedList.delete(91);
         circularLinkedList.add(10);
         circularLinkedList.add(50);
+        circularLinkedList.add(56);
         circularLinkedList.add(80);
         circularLinkedList.add(75);
+        circularLinkedList.display();
+        circularLinkedList.delete(56);
+        circularLinkedList.display();
+        circularLinkedList.delete(91);
         circularLinkedList.display();
     }
 }
